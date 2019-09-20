406c742bc6f36fa9b9098861e22ccff08608a31a
Otugeme Abel <aotugeme@gmail.com>
2019-09-20 08:45:06 +0100

    added an action to the form

--- contact.php
+++ contact.php
@@ -1,7 +1,17 @@
-<<?php  include "includes/header.php"; ?>
+<<?php  include "includes/header.php"; ?> <div class="wrapper ">
+
+    <div class="container">
+        <form id="upload-form" action="includes/contact.inc.php" method="POST" enctype="multipart/form-data">
+
+            <input type="text" name="name" placeholder="Name"><br>
+            <input type="text" name="email" placeholder="Email"><br>
+            <textarea type="text" name="message" placeholder="Message "></textarea><br>
+            <button class="btn btn-primary" type="submit" name="submit">SEND</button>
+
+        </form>
+    </div>
+
+    </div>
 
 
-<div class="wrapper "></div>
-
-
-<<?php  include "includes/footer.php"; ?>
+    <<?php  include "includes/footer.php"; ?>
